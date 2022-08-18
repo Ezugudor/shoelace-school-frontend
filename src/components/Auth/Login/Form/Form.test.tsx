e8dad3ed99de640261172531a1641f0e1c9c7f77
ezugudor <nelsonsmrt@gmail.com>
2022-08-18 06:41:21 +0100

    Added Login Page and Login Form component tests

--- src/components/Auth/Login/Form/Form.test.tsx
+++ src/components/Auth/Login/Form/Form.test.tsx
@@ -1,9 +1,51 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Form from "./Form";
+import { UserLogin } from "../../../../models/User";
 
-it("renders learn react link", () => {
-  // render(<NewSchoolForm />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe("Login Page Form Component", () => {
+  const MockSubmit = jest.fn();
+  const MockSetFieldValue = jest.fn();
+
+  it("Username field: calls onchange handler", () => {
+    render(
+      <Form
+        submit={MockSubmit}
+        setFieldValue={MockSetFieldValue}
+        credentials={{} as UserLogin}
+      />
+    );
+
+    const field = screen.getByPlaceholderText(/username/i);
+    fireEvent.change(field, { target: { value: "h" } });
+    fireEvent.change(field, { target: { value: "hi" } });
+    expect(MockSetFieldValue).toHaveBeenCalledTimes(2);
+  });
+
+  it("Password field: calls onchange handler", () => {
+    render(
+      <Form
+        submit={MockSubmit}
+        setFieldValue={MockSetFieldValue}
+        credentials={{} as UserLogin}
+      />
+    );
+    const field = screen.getByPlaceholderText(/username/i);
+    fireEvent.change(field, { target: { value: "h" } });
+    fireEvent.change(field, { target: { value: "hi" } });
+    expect(MockSetFieldValue).toHaveBeenCalledTimes(2);
+  });
+
+  it("calls submit handler", () => {
+    render(
+      <Form
+        submit={MockSubmit}
+        setFieldValue={MockSetFieldValue}
+        credentials={{} as UserLogin}
+      />
+    );
+    const btn = screen.getByText(/login/i);
+    fireEvent.click(btn);
+    expect(MockSubmit).toHaveBeenCalledTimes(1);
+  });
 });
--- src/components/Auth/Login/Login.test.tsx
+++ src/components/Auth/Login/Login.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Login } from "./Login";
+import { BrowserRouter } from "react-router-dom";
 
-it("renders learn react link", () => {
-  // render(<Login />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe("Login Page Component", () => {
+  const MockLogin = () => (
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
+  it("renders Username Field", () => {
+    render(<MockLogin />);
+    const field = screen.getByPlaceholderText(/username/i);
+    expect(field).toBeInTheDocument();
+  });
+
+  it("renders Password Field", () => {
+    render(<MockLogin />);
+    const field = screen.getByPlaceholderText(/password/i);
+    expect(field).toBeInTheDocument();
+  });
+
+  it("renders Login  Button", () => {
+    render(<MockLogin />);
+    const btn = screen.getByText(/login/i);
+    expect(btn).toBeInTheDocument();
+  });
 });
