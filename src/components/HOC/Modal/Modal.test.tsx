c8fbb8316f55a14205cad31168b8767115c5d0dd
ezugudor <nelsonsmrt@gmail.com>
2022-08-19 04:45:50 +0100

    modal test

--- src/components/HOC/Modal/Modal.test.tsx
+++ src/components/HOC/Modal/Modal.test.tsx
@@ -1,9 +1,47 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Modal from "./Modal";
 
-it("renders learn react link", () => {
-  // render(<Modal />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe("Modal Component", () => {
+  const MockClick = jest.fn();
+  it("renders Modal component with title", () => {
+    render(
+      <Modal show={true} title="Title" click={MockClick}>
+        <span>Child</span>
+      </Modal>
+    );
+    const elem = screen.getByText(/title/i);
+    expect(elem).toBeInTheDocument();
+  });
+
+  it("renders Modal component with children", () => {
+    render(
+      <Modal show={true} title="Title" click={MockClick}>
+        <span>Child</span>
+      </Modal>
+    );
+    const elem = screen.getByText(/child/i);
+    expect(elem).toBeInTheDocument();
+  });
+
+  it("should not render modal if prop 'show' is false", () => {
+    render(
+      <Modal show={false} title="Title" click={MockClick}>
+        <span>Child</span>
+      </Modal>
+    );
+    const elem = screen.queryByText(/child/i);
+    expect(elem).not.toBeInTheDocument();
+  });
+
+  it("calls modal overlay handler", () => {
+    render(
+      <Modal show={true} title="Title" click={MockClick}>
+        <span>Child</span>
+      </Modal>
+    );
+    const overlay = screen.getByTestId(/overlay/i);
+    fireEvent.click(overlay);
+    expect(MockClick).toHaveBeenCalledTimes(1);
+  });
 });
--- src/components/HOC/Modal/Modal.tsx
+++ src/components/HOC/Modal/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 
 const Modal: FC<ModalProps> = props => {
   return props.show ? (
-    <div className={styles.Overlay} onClick={props.click}>
+    <div className={styles.Overlay} onClick={props.click} data-testid="overlay">
       <div className={styles.Container} onClick={e => e.stopPropagation()}>
         <header>{props.title}</header>
         <div>{props.children}</div>
